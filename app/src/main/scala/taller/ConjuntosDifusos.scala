be7a10cb0c4e95ab74b0cfaf9252bf527d513f6c
Miguel Uribe <miguel.uribe@correounivalle.edu.co>
2025-10-04 20:36:53 -0500

    Función grande terminada, faltan las otras 5

--- app/src/main/scala/taller/ConjuntosDifusos.scala
+++ app/src/main/scala/taller/ConjuntosDifusos.scala
@@ -5,10 +5,15 @@ class ConjuntosDifusos() {
   def pertenece(elem: Int, s: ConjDifuso): Double = {
     s(elem)
   }
+
   def grande(d: Int, e: Int): ConjDifuso = {
-    // Implementaci´on de la funci´on grande
-  ...
+    //Comprobaciones de que e y d esten en el rango ideal
+    if (d < 1) throw new IllegalArgumentException("d debe ser mayor o igual que 1")
+    if (e <= 1) throw new IllegalArgumentException("e debe ser mayor que 1")
+    //Aplicamos la formula de grande
+    (n: Int) => math.pow(n.toDouble / (n + d).toDouble, e.toDouble)
   }
+
   def complemento(c: ConjDifuso): ConjDifuso = {
     // Implementaci´on de la funci´on complemento
   ...
